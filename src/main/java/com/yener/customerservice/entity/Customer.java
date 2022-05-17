8124133097e20ffd1a2091e2db14b0e337ee5719
yenerhan <yenerhan@gmail.com>
2022-05-17 15:03:00 +0300

    Geliştirme eklendi.

--- src/main/java/com/yener/customerservice/entity/Customer.java
+++ src/main/java/com/yener/customerservice/entity/Customer.java
@@ -1,8 +1,6 @@
 package com.yener.customerservice.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -10,6 +8,7 @@ import javax.persistence.*;
 @Table(name = "customer")
 @Getter
 @Setter
+@AllArgsConstructor
 @ToString
 public class Customer {
 
